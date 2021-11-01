dadac71ccc8b9455ff7a3fd869e377f8b308014d
ClaudioMandujano <claudio.mandujano@gmail.com>
2021-11-01 18:36:00 -0300

    Panel de administrado terminado

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../models/bd');
+var novedadesModel = require('../models/novedadesModel');
 
+
+router.get('/', async function(req, res, next){
+  novedades = await novedadesModel.getNovedades();
+  novedades = novedades.splice(0,5);
+  res.render('index',{
+    novedades
+  });
+});
 
 
 router.get('/', function(req, res, next) {
